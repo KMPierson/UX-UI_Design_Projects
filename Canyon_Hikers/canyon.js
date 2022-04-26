a389b57c76c945c55c71367a91d65f46b303fed2
Kiera Pierson <kieramarie_32@hotmail.com>
2022-04-26 18:54:28 -0400

    canyon assignement

---
+++ Canyon_Hikers/canyon.js
@@ -0,0 +1,13 @@
+// opening and closing the contact form
+function openForm() {
+    document.getElementById("myForm").style.display= "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function play(){
+    var audio=document.getElementById('sound');
+    audio.play();
+}
